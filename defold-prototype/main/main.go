25d6610024743eacaa02843aea0cca4de7db1d86
aseel salah <aseel.salah90@yahoo.com>
2026-09-12 17:19:01 +0200

    defold: wire native success audio

--- defold-prototype/main/main.go
+++ defold-prototype/main/main.go
@@ -13,3 +13,18 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "well_done"
+  component: "/audio/well_done.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
